043c164aea58e0d063065b5495c4cf5837514b80
Aarom Cárdenas <madafaka9609@gmail.com>
2022-09-16 14:40:33 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+require("dotenv").config({ path: ".development.env" });
+
+//SERVER CREATION:
+const app = express();
+
+//MIDDLEWARES:
+app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
+
+//ROUTES:
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Hello World!" });
+});
+
+//SERVER RUNING:
+app.listen(process?.env.SERVER_PORT || 3000, () => {
+  console.log(`Server running at port ${process.env.SERVER_PORT}`);
+});
